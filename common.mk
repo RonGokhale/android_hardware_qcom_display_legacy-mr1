6ade025a2576367d3e3f91b251796cf71de74604
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-23 18:49:54 +0000

    legacy-mr1: Fix up includes
    Change-Id: I5f534abf0c85d070dacac9089f1d1ee4be71c231

--- common.mk
+++ common.mk
@@ -1,12 +1,12 @@
 #Common headers
-common_includes := hardware/qcom/display/libgralloc
-common_includes += hardware/qcom/display/libgenlock
-common_includes += hardware/qcom/display/liboverlay
-common_includes += hardware/qcom/display/libcopybit
-common_includes += hardware/qcom/display/libqdutils
-common_includes += hardware/qcom/display/libhwcomposer
-common_includes += hardware/qcom/display/libexternal
-common_includes += hardware/qcom/display/libqservice
+common_includes := hardware/qcom/display_legacy-mr1/libgralloc
+common_includes += hardware/qcom/display_legacy-mr1/libgenlock
+common_includes += hardware/qcom/display_legacy-mr1/liboverlay
+common_includes += hardware/qcom/display_legacy-mr1/libcopybit
+common_includes += hardware/qcom/display_legacy-mr1/libqdutils
+common_includes += hardware/qcom/display_legacy-mr1/libhwcomposer
+common_includes += hardware/qcom/display_legacy-mr1/libexternal
+common_includes += hardware/qcom/display_legacy-mr1/libqservice
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
