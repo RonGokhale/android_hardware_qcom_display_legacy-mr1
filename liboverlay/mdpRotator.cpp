5e5034576ed86d931ed8d2abdd8d68bf1b66c0e2
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-23 19:22:49 +0000

    display: Remove secure playback
    Change-Id: If2ca4f14671382c964efc9027623b3b63e77c0c9

--- liboverlay/mdpRotator.cpp
+++ liboverlay/mdpRotator.cpp
@@ -54,9 +54,6 @@ void MdpRot::setSource(const overlay::utils::Whf& awhf) {
 }
 
 void MdpRot::setFlags(const utils::eMdpFlags& flags) {
-    mRotImgInfo.secure = 0;
-    if(flags & utils::OV_MDP_SECURE_OVERLAY_SESSION)
-        mRotImgInfo.secure = 1;
 }
 
 void MdpRot::setTransform(const utils::eTransform& rot, const bool& rotUsed)
@@ -99,7 +96,7 @@ bool MdpRot::open_i(uint32_t numbufs, uint32_t bufsz)
 
     OVASSERT(MAP_FAILED == mem.addr(), "MAP failed in open_i");
 
-    if(!mem.open(numbufs, bufsz, mRotImgInfo.secure)){
+    if(!mem.open(numbufs, bufsz, 0)){
         ALOGE("%s: Failed to open", __func__);
         mem.close();
         return false;
